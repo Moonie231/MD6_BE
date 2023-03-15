32c00b1c914871b90ae519ff1f2b951f8fd82c36
Đamthanhtung <tungdtha140465@fpt.edu.vn>
2023-03-15 10:16:11 +0700

    pull

--- dist/src/controller/foodController.d.ts
+++ dist/src/controller/foodController.d.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 declare class FoodController {
     private foodService;
     constructor();
+    getAll: (req: Request, res: Response) => Promise<void>;
     getAllFood: (req: Request, res: Response) => Promise<void>;
     getMyFood: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
     findCategory: (req: Request, res: Response) => Promise<void>;
